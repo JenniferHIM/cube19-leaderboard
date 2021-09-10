a3a04b204335343fe18a0994b4ba29099402be6d
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-09-10 10:14:00 +0300

    add LeadersItem logic

--- src/components/LeadersList/LeadersList.tsx
+++ src/components/LeadersList/LeadersList.tsx
@@ -1,18 +1,31 @@
 import {useState} from 'react';
-import {useDispatch} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {modalAddLeadersActions, modalEditLeadersActions} from 'redux/modal/modal-actions';
 import {ILeader} from '../../redux/leaders/interfaces/index';
+import LeadersItem from '../LeadersItem/LeadersItem';
+import {sortAllLeaders} from '../../redux/leaders/leaders-selectors';
+import {modalAddLeadersSelectors, modalEditLeadersSelectors} from '../../redux/modal/modal-selectors';
 import styles from './LeadersList.module.scss';
 
 const LeadersList = () => {
   const dispatch = useDispatch();
   const [leader, setLeader] = useState<ILeader>({name: '', rank: 0, score: 0});
+  const leaders = useSelector(sortAllLeaders);
+
+  const onToggleAddModal = () => dispatch(modalAddLeadersActions());
+
+  const handleUpdateLeader = (leader: ILeader) => {
+    dispatch(modalEditLeadersActions());
+    setLeader(leader);
+  };
 
   return (
     <div className={styles.leaderList}>
       <h2 className={styles.leaderList__title}>Leaders table for this period</h2>
-      <button type="button" className={styles.leaderList__button}>
+      <button type="button" className={styles.leaderList__button} onClick={onToggleAddModal}>
         + Add new score
       </button>
+      <LeadersItem leaders={[]} />
     </div>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import leaders from './leaders/leaders-reducer';
+import {isModalAddLeaders, isModalEditLeaders} from './modal/modal-reducer';
 
 const store = configureStore({
   reducer: {
     leaders,
+    isModalAddLeaders,
+    isModalEditLeaders,
   },
   devTools: process.env.NODE_ENV === 'development',
 });
