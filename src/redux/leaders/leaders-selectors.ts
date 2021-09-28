59c9084a6214bf5b11cc3c783b92ce37978290da
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-09-28 14:12:57 +0300

    add logic

--- src/redux/leaders/leaders-selectors.ts
+++ src/redux/leaders/leaders-selectors.ts
@@ -1,7 +1,13 @@
-import {ILeader} from './interfaces';
 import {RootState} from '../store';
 
 export const getAllLeaders = (state: RootState) => state.leaders;
 
 export const sortAllLeaders = (state: RootState) =>
   state.leaders.sort((a, b) => b.score - a.score).map((leader, idx: number) => ({...leader, rank: idx + 1, change: 0}));
+
+export const getHighScoreLeaders = (state: RootState) => {
+  const allLeaders = getAllLeaders(state)
+    .flat(2)
+    .sort((a, b) => b.score - a.score);
+  return allLeaders.slice(0, 4);
+};
