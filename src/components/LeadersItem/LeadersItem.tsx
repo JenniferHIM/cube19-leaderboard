de578f9e66d2916ee51ff3bf01ec442ae3f4d665
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2022-01-04 12:54:52 +0200

    add dev branch

--- src/components/LeadersItem/LeadersItem.tsx
+++ src/components/LeadersItem/LeadersItem.tsx
@@ -11,45 +11,21 @@ type LeadersItemProps = {
   editLeader: (param: ILeader) => void;
 };
 
-const listStyles = {
-  green: styles.leadersItem__green,
-  yellow: styles.leadersItem__yellow,
-  red: styles.leadersItem__red,
-};
+enum listStyles {
+  green,
+  yellow,
+  red,
+}
 
 const LeadersItem: FC<LeadersItemProps> = (props: LeadersItemProps) => {
   const {leader, editLeader} = props;
-  const changeSort = (rank: number) => {
-    switch (rank) {
-      case 1:
-        return 'st';
-      case 2:
-        return 'nd';
-      case 3:
-        return 'rd';
-      default:
-        return 'th';
-    }
-  };
   return (
-    <div className={styles.wrapper}>
-      <li key={uuidv4()} className={styles.leadersItem__item}>
-        <div className={styles.leadersItem__place}>
-          {leader.rank}
-          {changeSort(leader.rank)}
-        </div>
+    <div className={styles.leadersItem}>
+      <li className={styles.leadersItem__item}>
         <img className={styles.leadersItem__img} src={LeaderImg} alt="leader" />
         <p className={styles.leadersItem__score}>{leader.score}</p>
         <p className={styles.leadersItem__name}>{leader.name}</p>
-        <div
-          className={cn({
-            [listStyles.yellow]: leader.change === 0,
-            [listStyles.green]: leader.change > 0,
-            [listStyles.red]: leader.change < 0,
-          })}
-        >
-          {leader.change === 0 ? 'No Change' : `${Math.abs(leader.change)} place`}
-        </div>
+        <div>{leader.change === 0 ? 'No Change' : `${leader.change}place`}</div>
         <img className={styles.leadersItem__edit} src={PencilImg} alt="pencil" onClick={() => editLeader(leader)} />
       </li>
     </div>
